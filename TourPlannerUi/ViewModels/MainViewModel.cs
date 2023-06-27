59f6ddcf09e22217b5ca37b25df5397c7822f32f
lanaklanaA <if21b072@technikum-wien.at>
2023-06-27 21:32:42 +0200

    Add connection to MapQuestAPI

--- TourPlannerUi/ViewModels/MainViewModel.cs
+++ TourPlannerUi/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace TourPlannerUi.ViewModels {
     public class MainViewModel {
         private ToursViewModel _toursViewModel;
         private CreateAndEditTourViewModel _createAndEditTourViewModel;
+        private MapViewModel _mapViewModel;
 
         public MainViewModel() {
             _toursViewModel = new ToursViewModel();
             _createAndEditTourViewModel = new CreateAndEditTourViewModel();
+            _mapViewModel = new MapViewModel();
         }
     }
 }
