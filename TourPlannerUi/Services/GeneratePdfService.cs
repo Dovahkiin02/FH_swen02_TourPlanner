7c38ec70becbb104b7ae4f731142dd0bc4cbebd6
lanaklanaA <if21b072@technikum-wien.at>
2023-07-07 02:54:37 +0200

    pdf nearly done, design missing

--- TourPlannerUi/Services/GeneratePdfService.cs
+++ TourPlannerUi/Services/GeneratePdfService.cs
@@ -8,12 +8,15 @@ using Document = iText.Layout.Document;
 using Paragraph = iText.Layout.Element.Paragraph;
 using iText.Kernel.Pdf.Canvas.Draw;
 using iText.IO.Font.Constants;
+using System.Collections.ObjectModel;
+using System.Text;
 
 namespace TourPlannerUi.Services
 {
     public interface IGeneratePdfService
     {
         void create(Tour tour);
+        void create(ObservableCollection<Tour> tours);
     }
 
     public class GeneratePdf : IGeneratePdfService
@@ -32,17 +35,17 @@ namespace TourPlannerUi.Services
             Paragraph tourNameParagraph = new Paragraph($"Tour: {tour.Name}")
                 .SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD))
                 .SetFontSize(16f)
-                .SetMarginTop(0f); // Setze den oberen Rand der Überschrift auf 0
+                .SetMarginTop(0f);
             document.Add(tourNameParagraph);
 
             foreach (TourLog log in tour.TourLogs)
             {
-                document.Add(new Paragraph($"Tour Id: {log.Id}"));
-                document.Add(new Paragraph($"Tour Date: {log.Date}"));
-                document.Add(new Paragraph($"Tour Difficulty: {log.Difficulty}"));
-                document.Add(new Paragraph($"Tour Duration: {log.Duration}"));
-                document.Add(new Paragraph($"Tour Rating: {log.Rating}"));
-                document.Add(new Paragraph($"Tour Comment: {log.Comment}"));
+                document.Add(new Paragraph($"Tour: {log.Id}"));
+                document.Add(new Paragraph($"Date: {log.Date}"));
+                document.Add(new Paragraph($"Difficulty: {log.Difficulty}"));
+                document.Add(new Paragraph($"Duration: {log.Duration}"));
+                document.Add(new Paragraph($"Rating: {log.Rating}"));
+                document.Add(new Paragraph($"Comment: {log.Comment}"));
 
                 document.Add(separator);
             }
@@ -53,10 +56,7 @@ namespace TourPlannerUi.Services
                 document.Add(new Paragraph("Map:"));
                 Image image = new Image(ImageDataFactory.Create(tour.MapImageUrl));
 
-                // Setze die Breite des Bildes auf die Breite des Seiteninhalts
-                image.SetWidth(document.GetPdfDocument().GetDefaultPageSize().GetWidth() - document.GetLeftMargin() - document.GetRightMargin());
-
-                // Platzierungsoptionen für das Bild
+                image.SetWidth(document.GetPdfDocument().GetDefaultPageSize().GetWidth() - document.GetLeftMargin() - document.GetRightMargin()); 
                 image.SetProperty(Property.FLOAT, FloatPropertyValue.LEFT);
                 image.SetMarginRight(10f);
 
@@ -65,6 +65,65 @@ namespace TourPlannerUi.Services
 
 
             document.Close();
+        }
+
+        public void create(ObservableCollection<Tour> tours)
+        {
+            string outputPath = $"..\\..\\..\\..\\generatedPdf\\{tours[0].Name}_tourSumLog.pdf";
+
+            PdfWriter writer = new(outputPath);
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+
+            LineSeparator separator = new LineSeparator(new SolidLine());
+            Paragraph separatorParagraph = new Paragraph().Add(separator);
+
+            StringBuilder title = new StringBuilder("Average from: ");
+
+            foreach (var x in tours)
+            {
+                title.Append($"{x.Name} - ");
+            }
+
+            Paragraph tourTitleParagraph = new Paragraph(title.ToString())
+                .SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD))
+                .SetFontSize(16f)
+                .SetMarginTop(0f); 
+            document.Add(tourTitleParagraph);
+
+
+
+            foreach (var x in tours)
+            {
+                if (!string.IsNullOrEmpty(x.MapImageUrl))
+                {
+                    document.Add(new Paragraph("Map:"));
+                    Image image = new Image(ImageDataFactory.Create(x.MapImageUrl));
+
+                    image.SetWidth(document.GetPdfDocument().GetDefaultPageSize().GetWidth() - document.GetLeftMargin() - document.GetRightMargin());
+                    image.SetProperty(Property.FLOAT, FloatPropertyValue.LEFT);
+                    image.SetMarginRight(10f);
+
+                    document.Add(image);
+                }
+
+
+                foreach (var y in x.TourLogs)
+                {
+                    document.Add(new Paragraph($"Tour: {y.Id}"));
+                    document.Add(new Paragraph($"Date: {y.Date}"));
+                    document.Add(new Paragraph($"Difficulty: {y.Difficulty}"));
+                    document.Add(new Paragraph($"Duration: {y.Duration}"));
+                    document.Add(new Paragraph($"Rating: {y.Rating}"));
+                    document.Add(new Paragraph($"Comment: {y.Comment}"));
+                }
+
+                document.Add(separator);
+            }
+
+            document.Close();
+
+
         }
     }
 }
